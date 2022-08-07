dd293f64f63d10bcd46fab837844fba26a583220
s7kla4 <kar.vladi@yandex.ru>
2022-08-07 13:58:25 +0300

    prDnj
    .

---
+++ Assets/Scripts/AdderNew.cs
@@ -0,0 +1,271 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AdderNew: MonoBehaviour {
+	public static AdderNew instance = null;
+
+	void Awake() {
+		instance = this;
+    }
+
+	public float SideLength = 2f;
+	public int tileCounter = 0;
+
+	[System.Serializable]
+	public class meshSide {
+		public float height;
+		public float width;
+		public float distanseToCenter;
+
+		public List<int> Mask = new List<int>();
+		public int bPointIndex;
+
+		public Vector3 normal;
+		public Vector3 ort;
+		public Vector3 distanseToCenterPoint;
+		public List<Vector3> vertMesh = new List<Vector3>();
+	}
+
+	[System.Serializable]
+	public class tile {
+		public GameObject logic_tile;
+		public GameObject tile3d;
+		public Vector3 scale;
+		public int mass = 1;
+		public List<meshSide> side;
+	}
+
+	public List<tile> Holls = new List<tile>();
+
+	public void AdderNewStart() {
+		for (int i = 0; i < Holls.Count; i++) {
+			tileTrace(Holls[i]);
+			Holls[i].logic_tile.tag = "logTile";
+		}
+	}
+
+	void debDrowPoint(Vector3 point) {
+		float sc = 0.5f;
+		Debug.DrawLine(point - sc * Vector3.right, point + sc * Vector3.right, Color.red, 10000);
+		Debug.DrawLine(point - sc * Vector3.up, point + sc * Vector3.up, Color.red, 10000);
+		Debug.DrawLine(point - sc * Vector3.forward, point + sc * Vector3.forward, Color.red, 10000);
+	}
+
+	void getMaterial(tile tileBuf, meshSide meshBuf) {
+
+		GameObject inst = Instantiate(tileBuf.logic_tile);
+
+		Vector3 ort1 = vectProd(meshBuf.normal, Vector3.up);
+		Vector3 bufVect = new Vector3(100, 0, 0) * ort1.x;
+		Vector3 tileCenter = tileBuf.logic_tile.GetComponent<Renderer>().bounds.center;
+
+		meshBuf.ort = ort1;
+
+		for (int i = 0; i < meshBuf.vertMesh.Count; i++) {
+			if (meshBuf.vertMesh[i].x / ort1.x > bufVect.x / ort1.x) {
+				bufVect = meshBuf.vertMesh[i];
+				meshBuf.bPointIndex = i;
+			}
+		}
+
+		float distBuf = 0f;
+
+		for (int i = 0; i < meshBuf.vertMesh.Count; i++) {
+			float distVectX = Mathf.Pow((meshBuf.vertMesh[i].x - meshBuf.vertMesh[meshBuf.bPointIndex].x), 2f);
+			float distVectY = Mathf.Pow((meshBuf.vertMesh[i].y - meshBuf.vertMesh[meshBuf.bPointIndex].y), 2f);
+			float distVectZ = Mathf.Pow((meshBuf.vertMesh[i].z - meshBuf.vertMesh[meshBuf.bPointIndex].z), 2f);
+			float distVect = distVectX + distVectY + distVectZ;
+
+			if (distBuf < distVect) {
+				distBuf = distVect;
+				meshBuf.width = Mathf.Round(Mathf.Sqrt(distVectX + distVectZ) * tileBuf.scale.y / SideLength);
+				meshBuf.height = Mathf.Round(Mathf.Sqrt(distVectY) * tileBuf.scale.y / SideLength);
+			}
+		}
+
+		Ray rayBuf;
+		RaycastHit hit;
+
+		var meshCollider = tileBuf.logic_tile.GetComponent<MeshCollider>();
+
+		for (int i = 0; i < meshBuf.height; i++) {
+			for (int j = 0; j < meshBuf.width; j++) {
+
+				Vector3 start = tileCenter + meshBuf.vertMesh[meshBuf.bPointIndex] + (float)i * Vector3.up * SideLength + ((float)j) * ort1 * SideLength + 1.5f * meshBuf.normal;
+
+				//debDrowPoint(start);
+
+				rayBuf = new Ray(start, -meshBuf.normal);
+				Physics.Raycast(rayBuf, out hit, 4f);
+				//Debug.DrawRay(tileCenter + meshBuf.vertMesh[meshBuf.bPointIndex] + (float)i* Vector3.up * SideLength + ((float)j) * ort * SideLength + 0.5f * meshBuf.normal, meshBuf.normal, Color.green, 10000);
+
+				if (hit.collider == null) {
+					Debug.DrawLine(start, start - 4f * meshBuf.normal, Color.green, 10000);
+					//Debug.Log("boba aboba");
+					continue;
+				}
+
+				Mesh mesh = meshCollider.sharedMesh;
+				int materialInd = -1;
+				int index = hit.triangleIndex;
+
+				for (int t = 0; t < mesh.subMeshCount; t++) {
+					var bufTriangles = mesh.GetTriangles(t); 
+					for(int k = 0; k < bufTriangles.Length; k++) {
+						if(index == bufTriangles[k]) {
+							materialInd = t;
+							break;
+                        }
+                    }
+					if (materialInd != -1)
+						break;
+                }
+
+				//Debug.Log(index);
+				//Debug.Log(materialInd);
+				//Debug.Log(tileBuf.logic_tile.GetComponent<Renderer>().materials[materialInd]);
+
+				meshBuf.Mask.Add(materialInd);
+			}
+		}
+
+		Destroy(inst);
+	}
+
+	Vector3 getNormal(Vector3 p0, Vector3 p1, Vector3 p2) {
+		Vector3 norm;
+
+		Vector3 v1 = p1 - p0;
+		Vector3 v2 = p2 - p0;
+
+		norm.x = v1.y * v2.z - v1.z * v2.y;
+		norm.y = v1.z * v2.x - v1.x * v2.z;
+		norm.z = v1.x * v2.y - v1.y * v2.x;
+
+		return norm;
+	}
+
+	Vector3 vectProd(Vector3 a, Vector3 b) {
+		Vector3 outp = new Vector3();
+		outp.x = a.y * b.z - a.z * b.y;
+		outp.y = a.z * b.x - a.x * b.z;
+		outp.z = a.x * b.y - a.y * b.x;
+		return outp;
+	}
+	private static int comp(Vector3 a, Vector3 b) {
+		if (a.y < b.y) {
+			return -1;
+		} else if (a.y == b.y) {
+			if (a.x < b.x) {
+				return -1;
+			} else if (a.x == b.x) {
+				if (a.z < b.z) {
+					return -1;
+				} else if (a.z == b.z) {
+					return 0;
+				}
+			}
+		}
+		return 1;
+	}
+
+
+	void tileTrace(tile tileBuf) {
+		GameObject inst = Instantiate(tileBuf.logic_tile);
+
+		tileBuf.scale = tileBuf.tile3d.transform.localScale;
+
+		var meshCollaider = tileBuf.logic_tile.GetComponent<MeshCollider>();
+		Mesh mesh = meshCollaider.sharedMesh;
+		Vector3 norm;
+
+		float rayDistance = 4f;
+
+		Vector3 endTrace;
+		Vector3 tileCenter = tileBuf.logic_tile.GetComponent<Renderer>().bounds.center;
+
+		RaycastHit hit;
+
+		Ray rayTrace;
+		for (float i = 0; i < 102; i++) {
+			endTrace.x = Mathf.Sin(i / 51f * Mathf.PI) * rayDistance;
+			endTrace.z = Mathf.Cos(i / 51f * Mathf.PI) * rayDistance;
+			endTrace.y = 0f;
+
+			rayTrace = new Ray(tileCenter + endTrace, -endTrace);
+
+			Physics.Raycast(tileCenter + endTrace, -endTrace, out hit, rayDistance);
+
+			if (hit.collider == null) {
+				continue;
+			}
+
+			Vector3[] vertices = mesh.vertices;
+			int[] triangles = mesh.triangles;
+
+			Vector3 p0 = vertices[triangles[hit.triangleIndex * 3 + 0]];
+			Vector3 p1 = vertices[triangles[hit.triangleIndex * 3 + 1]];
+			Vector3 p2 = vertices[triangles[hit.triangleIndex * 3 + 2]];
+
+			norm = getNormal(tileBuf.scale.x * p0, tileBuf.scale.x * p1, tileBuf.scale.x * p2);
+
+			
+
+			if ((norm.x * endTrace.x) + (norm.y * endTrace.y) + (norm.z * endTrace.z) < 0) {
+				norm = -norm;
+			}
+
+			//norm.y = 0f;
+			norm = norm.normalized;
+
+			bool f = true;
+			bool f0 = true;
+			bool f1 = true;
+			bool f2 = true;
+
+			int sideCount = tileBuf.side.Count - 1;
+
+			for (int j = 0; (j < tileBuf.side.Count) && f; j++) {
+				if ((tileBuf.side[j].normal - norm).magnitude <= 0.001f) {
+					f = false;
+					sideCount = j;
+
+				}
+			}
+
+			if (f) {
+				tileBuf.side.Add(new meshSide());
+				sideCount++;
+				tileBuf.side[sideCount].normal = norm;
+				//Debug.Log(tileBuf.side[sideCount].normal);
+				tileBuf.side[sideCount].distanseToCenter = Mathf.Abs((norm.x * tileCenter.x + norm.y * tileCenter.y + tileCenter.z * p0.z) - (norm.x * p0.x + norm.y * p0.y + norm.z * p0.z));
+				tileBuf.side[sideCount].distanseToCenterPoint = tileBuf.side[tileBuf.side.Count - 1].distanseToCenter * norm + tileCenter;
+			}
+
+			for (int j = 0; j < tileBuf.side[sideCount].vertMesh.Count; j++) {
+				if ((p0 - tileBuf.side[sideCount].vertMesh[j]).magnitude <= 0.001f)
+					f0 = false;
+				if ((p1 - tileBuf.side[sideCount].vertMesh[j]).magnitude <= 0.001f)
+					f1 = false;
+				if ((p2 - tileBuf.side[sideCount].vertMesh[j]).magnitude <= 0.001f)
+					f2 = false;
+			}
+
+			if (f0)
+				tileBuf.side[sideCount].vertMesh.Add(p0);
+			if (f1)
+				tileBuf.side[sideCount].vertMesh.Add(p1);
+			if (f2)
+				tileBuf.side[sideCount].vertMesh.Add(p2);
+		}
+
+		Destroy(inst);
+
+		for (int i = 0; i < tileBuf.side.Count; i++) {
+			getMaterial(tileBuf, tileBuf.side[i]);
+		}
+	} 
+}
+
+
---
+++ Assets/Scripts/Generator.cs
@@ -0,0 +1,182 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class Generator : MonoBehaviour {
+
+	public int iterGen = 5;
+	public int cntTile = -1;
+
+	public Material neitralMaterial;
+	public Material startMaterial;
+
+	public bool upd;
+	
+	public float sideLength;
+
+	[System.Serializable]
+	public class meshSide {
+		public float height;
+		public float width;
+		public float distanseToCenter;
+
+		public List<int> Mask = new List<int>();
+		public int bPointIndex;
+
+		public Vector3 normal;
+		public Vector3 ort;
+		public Vector3 distanseToCenterPoint;
+		public List<Vector3> vertMesh = new List<Vector3>();
+	}
+
+	[System.Serializable]
+	public class tile {
+		public GameObject logic_tile;
+		public GameObject tile3d;
+		public Vector3 scale;
+		public int mass = 1;
+		public List<meshSide> side = new List<meshSide>();
+	}
+
+	public List<tile> Holls = new List<tile>();
+	public List<int> massDung = new List<int>();
+
+	void Start () {
+		sideLength = AdderNew.instance.SideLength;
+
+		if (upd) {
+			AdderNew.instance.AdderNewStart();
+			for (int i = 0; i < AdderNew.instance.Holls.Count; i++) {
+				Holls.Add(new tile());
+				Holls[i].logic_tile = AdderNew.instance.Holls[i].logic_tile;
+				Holls[i].tile3d = AdderNew.instance.Holls[i].tile3d;
+                Holls[i].mass = AdderNew.instance.Holls[i].mass;
+                Holls[i].scale = AdderNew.instance.Holls[i].scale;
+				for(int j = 0; j < AdderNew.instance.Holls[i].side.Count; j++) {
+                    Holls[i].side.Add(new meshSide());
+                    Holls[i].side[j].normal = AdderNew.instance.Holls[i].side[j].normal;
+					Holls[i].side[j].height = AdderNew.instance.Holls[i].side[j].height;
+					Holls[i].side[j].width = AdderNew.instance.Holls[i].side[j].width;
+                    Holls[i].side[j].Mask = AdderNew.instance.Holls[i].side[j].Mask;
+                    Holls[i].side[j].distanseToCenter = AdderNew.instance.Holls[i].side[j].distanseToCenter;
+					Holls[i].side[j].distanseToCenterPoint = AdderNew.instance.Holls[i].side[j].distanseToCenterPoint;
+                    Holls[i].side[j].vertMesh = AdderNew.instance.Holls[i].side[j].vertMesh;
+					Holls[i].side[j].ort = AdderNew.instance.Holls[i].side[j].ort;
+				}
+				massDung.Add(Holls[i].mass);
+			}
+        }
+
+		Gen(massDung, Vector3.up * 0.5f * sideLength, Vector3.forward, startMaterial);
+	}
+	
+	bool Gen(List<int> mas, Vector3 pos, Vector3 dir, Material mat) {
+		/*Ray ray = new Ray(pos - 0.1f * SideLength * dir, dir);
+		RaycastHit hit;
+		Physics.Raycast(Ray, SideLength * 0.5f, out hit);*/
+
+		bool usl = true;
+
+		int sumGen = 0;
+		int index = 0;
+		int massIndex;
+		
+		for (int i = 0; i < mas.Count; i++) {
+			sumGen += mas[i];
+		}
+		GameObject[] dung = GameObject.FindGameObjectsWithTag("logTile");
+
+		for (int u1 = 0; u1 < iterGen; u1++) {
+			//определяем случайный индекс
+			massIndex = Mathf.RoundToInt(sumGen * Random.value);
+			for (int i = 0; i < mas.Count; i++) {
+				if (massIndex <= mas[i]) {
+					index = i;
+					break;
+				}
+				massIndex -= mas[i];
+			}
+			//создаем объект
+			GameObject buf = Instantiate(Holls[index].logic_tile);
+			int sideIndex = -1;
+			int prohIndex = -1;
+			for (int u2 = 0; u2 < iterGen; u2++) {
+				usl = true;
+
+				List<int> posit = new List<int>();
+				int h, w;
+				int sideCnt = Holls[index].side.Count;
+				sideIndex = Mathf.RoundToInt(sideCnt * Random.value) % sideCnt;
+				for(int i = 0; i < Holls[index].side[sideIndex].Mask.Count; i++) {
+					int materialIndex = Holls[index].side[sideIndex].Mask[i];
+					if (Holls[index].logic_tile.GetComponent<Renderer>().materials[materialIndex] == mat) {
+						posit.Add(i);
+                    }
+                }
+				if(posit.Count == 0) {
+					if(u2 == iterGen - 1) {
+						usl = false;
+                    }
+					continue;
+                }
+				int positIndex = Mathf.RoundToInt(posit.Count * Random.value) % posit.Count;
+				prohIndex = positIndex;
+				h = positIndex % Mathf.RoundToInt(Holls[index].side[sideIndex].width);
+				w = (positIndex - h) / Mathf.RoundToInt(Holls[index].side[sideIndex].width);
+
+				float ang = Vector3.Angle(dir, Holls[index].side[positIndex].normal);
+
+				Quaternion tr = Quaternion.AngleAxis(ang, Vector3.up);
+				
+				Vector3 bufDir = (tr * Holls[index].side[positIndex].normal).normalized;
+				if ((dir - bufDir).magnitude > 0.01f) {
+					tr = Quaternion.AngleAxis(180 + ang, Vector3.up);
+				}
+
+				buf.transform.rotation = tr;
+
+				buf.transform.position = pos - ((h + 0.5f) * sideLength * Vector3.up + (w + 0.5f) * sideLength * Holls[index].side[positIndex].ort);
+
+				for(int i = 0; i < dung.Length; i++) {
+					float dist;
+					Vector3 dirBuf;
+					bool k = Physics.ComputePenetration(dung[i].GetComponent<Collider>(), dung[i].transform.position, dung[i].transform.rotation, buf.GetComponent<Collider>(), buf.transform.position, buf.transform.rotation, out dirBuf, out dist);
+					k |= (dist < 0.01f);
+					usl &= k;
+				}
+                if (!usl) {
+					continue;
+                }
+				for(int i = 0; i < Holls[index].side.Count; i++) {
+					for(int j = 0; j < Holls[index].side[i].Mask.Count; j++) {
+						if((i == sideIndex) && (j == prohIndex)) {
+							continue;
+                        }
+						int materialInd = Holls[index].side[i].Mask[j];
+						Material bufMaterial = Holls[index].logic_tile.GetComponent<Renderer>().materials[materialInd];
+						if (bufMaterial == neitralMaterial) {
+							continue;
+                        }
+						int h0, w0;
+						h0 = positIndex % Mathf.RoundToInt(Holls[index].side[sideIndex].width);
+						w0 = (positIndex - h0) / Mathf.RoundToInt(Holls[index].side[sideIndex].width);
+						Vector3 posOut = (0.5f + h0) * sideLength * Vector3.up + (0.5f + w0) * sideLength * Holls[index].side[sideIndex].ort + buf.transform.position;
+						Vector3 dirOut = (tr * Holls[index].side[sideIndex].normal).normalized;
+						usl &= Gen(mas, posOut, dirOut, bufMaterial); 
+                    }
+                }
+                if (usl) {
+					Destroy(buf);
+					Instantiate(Holls[index].tile3d);
+					cntTile++;
+					break;
+                }
+			}
+            if (!usl) {
+				return usl;
+            }
+		}
+		return false;
+    }
+}
